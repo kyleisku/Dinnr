2f36b6cccf62b446ba7f3e6490ab1f6571791d0b
Jason Le <Jasonleh99@gmail.com>
2018-05-09 21:54:09 -0700

    Limited input on reviews

--- app/src/main/java/com/csulb/cecsymemeteam/dinnr/WriteReviewActivity.java
+++ app/src/main/java/com/csulb/cecsymemeteam/dinnr/WriteReviewActivity.java
@@ -3,9 +3,12 @@ package com.csulb.cecsymemeteam.dinnr;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class WriteReviewActivity extends AppCompatActivity {
 
@@ -16,23 +19,55 @@ public class WriteReviewActivity extends AppCompatActivity {
         Button send = findViewById(R.id.writeReview_sendBtn);
         final EditText title = findViewById(R.id.writeReview_titleTxtField);
         final EditText comment = findViewById(R.id.writeReview_reviewTxtField);
+        final EditText Rating = findViewById(R.id.writeReview_ratingTxtField);
+        final TextView charactersLeft = findViewById(R.id.writeReview_maxCharactersTxt);
         send.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-                Review r = new Review("Anonymous","google.com",title.getText().toString(),
-                        comment.getText().toString(), 4);
-                if(DataStorage.isLoggedIn()){
-                    r.setProfileName(DataStorage.getLoginName());
+                if(Rating.getText().length() > 0) {
+                    Review r = new Review("Anonymous", "google.com", title.getText().toString(),
+                            comment.getText().toString(), processStarRating(Rating.getText() + ""));
+                    if (DataStorage.isLoggedIn()) {
+                        r.setProfileName(DataStorage.getLoginName());
+                    }
+                    DataStorage.getRestaurant().addReview(r, 0);
+                    sendReview(view);
                 }
-                DataStorage.getRestaurant().addReview(r, 0);
-                sendReview(view);
             }
-
         });
 
+        charactersLeft.setText("Characters Left: 500");
+
+        comment.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                charactersLeft.setText(charactersLeft.getText().subSequence(0, 17) + "" + (500 - comment.getText().length()));
+            }
+        });
     }
 
+    public static int processStarRating(String str){
+        int starRating = Integer.parseInt(str);
+        if(starRating > 5){
+            return 5;
+        }
+        else if(starRating < 0){
+            return 0;
+        }
+        else{
+            return starRating;
+        }
+    }
 
     public void goBack(View v){
         startActivity(new Intent(this, ProfileActivity.class));
